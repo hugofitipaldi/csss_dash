a90d012b83d2dfc3e61c48988c1d95ca8bdf8b64
Hugo Fitipaldi <hugofitipaldi@gmail.com>
2022-01-11 08:56:34 -0300

    maps pre-processing and data

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dash import html
 import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
+import geopandas as gpd
 
 app = dash.Dash()
 
@@ -12,24 +13,50 @@ colors = {
     'text': '#000080'
 }
 
-df = pd.read_csv(
+# Pre-processing -------------------------------------------------------------------------------
+# Data
+# line chart national
+national_preds = pd.read_csv(
     "https://raw.githubusercontent.com/csss-resultat/openData/main/datasets/nationella_senaste.csv"
 )
 
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=df.Datum, y=df.Uppskattning,mode='lines+markers', showlegend=False))
-fig.add_trace(go.Scatter(
+# National Map
+# Reading the shape file for map plotting
+gdf_map = gpd.read_file('data/scb/Lan_Sweref99TM_region.shp', encoding='utf-8')
+# Change the CRS to make Sweden thiner
+gdf_map = gdf_map.to_crs('EPSG:3021')
+gdf_map["Lan"] = ['Stockholm', 'Uppsala', 'Södermanland', 'Östergötland', 'Jönköping', 'Kronoberg', 'Kalmar',
+                         'Gotland', 'Blekinge', 'Skåne', 'Halland', 'Västra Götaland', 'Värmland', 'Örebro', 'Västmanland', 'Dalarna',
+                         'Gävleborg', 'Västernorrland',  'Jämtland', 'Västerbotten', 'Norrbotten']
+
+# Counties preds
+counties_preds = pd.read_csv("https://raw.githubusercontent.com/csss-resultat/openData/main/datasets/lan_senaste.csv"
+)
+counties_preds.Datum = pd.to_datetime(counties_preds["Datum"])
+latest_date = counties_preds["Datum"].max()
+counties_preds = counties_preds.loc[counties_preds["Datum"] == latest_date]
+counties_preds.Uppskattning = counties_preds.Uppskattning * 100
+counties_preds.Low_CI = counties_preds.Low_CI * 100
+counties_preds.High_CI = counties_preds.High_CI * 100
+
+gdf_plot = gdf_map.merge(counties_preds, left_on='Lan', right_on = 'Lan', how='left')
+
+# Plots ----------------------------------------------------------------------------------------
+
+lineplot_national = go.Figure()
+lineplot_national.add_trace(go.Scatter(x=national_preds.Datum, y=national_preds.Uppskattning, mode='lines+markers', showlegend=False, name ="% uppskattad förekomst"))
+lineplot_national.add_trace(go.Scatter(
     name='Upper Bound',
-    x=df['Datum'],
-    y=df['High_CI'],
+    x=national_preds['Datum'],
+    y=national_preds['High_CI'],
     mode='lines',
     marker=dict(color="rgba(255,255,255,0)"),
     showlegend=False)
 )
-fig.add_trace(go.Scatter(
+lineplot_national.add_trace(go.Scatter(
     name='Lower Bound',
-    x=df['Datum'],
-    y=df['Low_CI'],
+    x=national_preds['Datum'],
+    y=national_preds['Low_CI'],
     marker=dict(color="rgba(255,255,255,0)"),
     mode='lines',
     fillcolor='rgba(68, 68, 68, 0.3)',
@@ -38,17 +65,16 @@ fig.add_trace(go.Scatter(
 )
 
 
-fig.update_layout(title="",
-                  xaxis_title='Datum',
-                  yaxis_title="Uppskattad förekomst av symtomatisk covid-19",
-                  yaxis_range=[0,1],
-                  yaxis=dict(
+lineplot_national.update_layout(title="",
+                                xaxis_title='Datum',
+                                yaxis_title="Uppskattad förekomst av symtomatisk covid-19",
+                                yaxis_range=[0,1],
+                                yaxis=dict(
                       tickmode='array',
                       tickvals=[0,0.2, 0.4, 0.6, 0.8, 1.0],
                       ticktext=["0 %","O,2 %", "0,4 %", "0,6 %", "0,8 %", "1,0 %"]
                   )
-                  )
-
+                                )
 
 
 xaxis = dict(
@@ -73,7 +99,7 @@ app.layout = html.Div(children=[
             'color': colors['text']
         }
     ),
-    dcc.Graph(id="life-exp-vs-gdp", figure=fig)
+    dcc.Graph(id="life-exp-vs-gdp", figure=lineplot_national)
 ])
 
 
